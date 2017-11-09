ee568e83a65419e19442b60d6d4227c327e5be16
ManGoYTB <mangoytbfr@gmail.com>
2017-11-09 20:07:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -405,7 +405,38 @@ class Modmail(commands.Bot):
             await ctx.send('User successfully unblocked!')
         else:
             await ctx.send('User is not already blocked.')
+    
+    @checks.serverowner_or_permissions(administrator=True)
+    async def serverprefix(self, ctx, *prefixes):
+        """Sets Red's prefixes for this server
 
+        Accepts multiple prefixes separated by a space. Enclose in double
+        quotes if a prefix contains spaces.
+        Example: set serverprefix ! $ ? "two words"
+
+        Issuing this command with no parameters will reset the server
+        prefixes and the global ones will be used instead."""
+        server = ctx.message.server
+
+        if prefixes == ():
+            self.bot.settings.set_server_prefixes(server, [])
+            self.bot.settings.save_settings()
+            current_p = ", ".join(self.bot.settings.prefixes)
+            await self.bot.say("Server prefixes reset. Current prefixes: "
+                               "`{}`".format(current_p))
+            return
+
+        prefixes = sorted(prefixes, reverse=True)
+        self.bot.settings.set_server_prefixes(server, prefixes)
+        self.bot.settings.save_settings()
+        log.debug("Setting server's {} prefixes to:\n\t{}"
+                  "".format(server.id, self.bot.settings.prefixes))
+
+        p = "Prefixes" if len(prefixes) > 1 else "Prefix"
+        await self.bot.say("{} set for this server.\n"
+                           "To go back to the global prefixes, do"
+                           " `{}set serverprefix` "
+                           "".format(p, prefixes[0]))
                 
 if __name__ == '__main__':
     Modmail.init()
